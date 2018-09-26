c64233397afe1b43085d6471e4bfe47871d444cb
Kara Bayse <karabayse@gmail.com>
2018-09-26 13:48:27 -0500

    added router

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+// requires
 const Koa = require('koa');
+// npm install koa-router
+const KoaRouter = require('koa-router');
+// npm install koa-json
+const json = require('koa-json');
+
 const app = new Koa();
+const router = new KoaRouter();
 
 // uses
-app.use(async ctx => ctx.body = 'Hello World');
+// JSON Prettier Middleware
+app.use(json());
+
+// Simple Middleware Example
+// app.use(async ctx => (ctx.body = 'Hello World'));
+// app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
+
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server up'));
