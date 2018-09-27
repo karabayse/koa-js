34d56412885853d52a0936174b1a3ebb5339cb5f
Kara Bayse <karabayse@gmail.com>
2018-09-27 16:49:59 -0500

    added koa-ejs

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const Koa = require('koa');
 const KoaRouter = require('koa-router');
 // npm install koa-json
 const json = require('koa-json');
+// core Node module
+const path = require('path');
+// npm install koa-ejs
+const render = require('koa-ejs');
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -15,6 +19,10 @@ app.use(json());
 // Simple Middleware Example
 // app.use(async ctx => (ctx.body = 'Hello World'));
 // app.use(async ctx => (ctx.body = { msg: 'Hello World' }));
+
+render(app, {
+
+});
 
 router.get('/test', ctx => (ctx.body = 'Hello Test'));
 
